5547e2c682b302aa31da6577300f7a52fb0dbfb9
이성희 <>
2021-02-09 18:48:53 +0900

    에디터도 한번 만들어보자 - 크기 조절 쉽게 const로 뺐음. - 보기 쉽게 구분선 추가 - State표시

--- BehaviorTree_Test/Assets/BehaviorTree/Scripts/Editor/BehaviorTreeEditor_DrawGraph.cs
+++ BehaviorTree_Test/Assets/BehaviorTree/Scripts/Editor/BehaviorTreeEditor_DrawGraph.cs
@@ -9,6 +9,11 @@ namespace BehaviorTree
 {
     public partial class BehaviorTreeEditor : EditorWindow
     {
+        const float GRAPH_WINDOW_NAME_WIDTH = 100;
+        const float GRAPH_WINDOW_VALUE_WIDTH = 100;
+        const float GRAPH_SPACE_X = 50;
+        const float GRAPH_SPACE_Y = 50;
+
         public void DrawGraph(NodeBase rootNode, bool isEditable = false)
         {
             GUILayout.Space(10); // 살짝 공간만 만들어주자.
@@ -20,8 +25,10 @@ namespace BehaviorTree
             EditorGUILayout.BeginVertical();
             {
                 Rect parentRect;
+                var children = node.GetAllChildren();
                 var fields = node.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
                 Dictionary<string, object> changedValues = new Dictionary<string, object>();
+                
 
                 EditorGUILayout.BeginHorizontal();
                 GUILayout.FlexibleSpace();
@@ -44,17 +51,23 @@ namespace BehaviorTree
 
                     if (node is NodeRoot)
                     {
-                        GUILayout.Label("", GUILayout.Width(100));
+                        GUILayout.Label("", GUILayout.Width(GRAPH_WINDOW_NAME_WIDTH + GRAPH_WINDOW_VALUE_WIDTH));
                     }
                     else
                     {
                         EditorGUILayout.BeginHorizontal();
-                        GUILayout.Label("Name", GUILayout.Width(100));
-                        var changedName = EditorGUILayout.TextField(node.Name, GUILayout.Width(100));
+                        GUILayout.Label("Name", GUILayout.Width(GRAPH_WINDOW_NAME_WIDTH));
+                        var changedName = EditorGUILayout.TextField(node.Name, GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         if (changedName != node.Name) changedValues.Add("Name", changedName);
                         EditorGUILayout.EndHorizontal();
-                        GUILayout.Space(10);
                     }
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Label("State", GUILayout.Width(GRAPH_WINDOW_NAME_WIDTH));
+                    // EditorGUILayout.TextField(node.State.ToString(), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
+                    GUILayout.Label(node.State.ToString(), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
+                    EditorGUILayout.EndHorizontal();
+                    GUILayout.Space(10);
 
                     for (int i = 0; i < fields.Length; i ++)
                     {
@@ -67,24 +80,24 @@ namespace BehaviorTree
                         if (nodeAttr.Option != NodeAttributeOptionType.Required) continue;
 
                         EditorGUILayout.BeginHorizontal();
-                        GUILayout.Label(nodeAttr.Name, GUILayout.Width(100));
+                        GUILayout.Label(nodeAttr.Name, GUILayout.Width(GRAPH_WINDOW_NAME_WIDTH));
                         object changedValue = null;
                         if (fields[i].FieldType == typeof(string))
-                            changedValue = EditorGUILayout.TextField((string)fields[i].GetValue(node), GUILayout.Width(100));
+                            changedValue = EditorGUILayout.TextField((string)fields[i].GetValue(node), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         else if (fields[i].FieldType == typeof(int))
-                            changedValue = EditorGUILayout.IntField((int)fields[i].GetValue(node), GUILayout.Width(100));
+                            changedValue = EditorGUILayout.IntField((int)fields[i].GetValue(node), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         else if (fields[i].FieldType == typeof(long))
-                            changedValue = EditorGUILayout.LongField((long)fields[i].GetValue(node), GUILayout.Width(100));
+                            changedValue = EditorGUILayout.LongField((long)fields[i].GetValue(node), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         else if (fields[i].FieldType == typeof(float))
-                            changedValue = EditorGUILayout.FloatField((float)fields[i].GetValue(node), GUILayout.Width(100));
+                            changedValue = EditorGUILayout.FloatField((float)fields[i].GetValue(node), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         else if (fields[i].FieldType == typeof(double))
-                            changedValue = EditorGUILayout.DoubleField((double)fields[i].GetValue(node), GUILayout.Width(100));
+                            changedValue = EditorGUILayout.DoubleField((double)fields[i].GetValue(node), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         else if (fields[i].FieldType == typeof(bool))
-                            changedValue = EditorGUILayout.Toggle((bool)fields[i].GetValue(node), GUILayout.Width(100));
+                            changedValue = EditorGUILayout.Toggle((bool)fields[i].GetValue(node), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         else if (fields[i].FieldType.IsEnum)
-                            changedValue = EditorGUILayout.EnumPopup((Enum)fields[i].GetValue(node), GUILayout.Width(100));
+                            changedValue = EditorGUILayout.EnumPopup((Enum)fields[i].GetValue(node), GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         else
-                            GUILayout.Label(fields[i].FieldType.Name, GUILayout.Width(100));
+                            GUILayout.Label(fields[i].FieldType.Name, GUILayout.Width(GRAPH_WINDOW_VALUE_WIDTH));
                         EditorGUILayout.EndHorizontal();
 
                         if (changedValue != null)
@@ -126,40 +139,44 @@ namespace BehaviorTree
                     }
                 }
 
-                GUILayout.Space(50);
-
-                var children = node.GetAllChildren();
-                EditorGUILayout.BeginHorizontal();
-                for (int i = 0; i < children.Length; i ++)
+                if (children.Length > 0)
                 {
-                    GUILayout.Space(50);
-                    DrawGraphRecusively(children[i], isEditable);
-                    Rect childRect = GUILayoutUtility.GetLastRect();
-                    Vector3 startPos = new Vector3(
-                            parentRect.center.x, 
-                            parentRect.center.y + parentRect.height * 0.5f, 
-                            0);
-                    Vector3 endPos = new Vector3(
-                            childRect.center.x, 
-                            childRect.center.y - childRect.height * 0.5f, 
-                            0);
-                    if (EditorGUIUtility.isProSkin == false) //outline
-                    {
-                        Handles.DrawBezier(startPos, endPos,
-                        new Vector3(startPos.x, endPos.y, 0),
-                        new Vector3(endPos.x, startPos.y, 0),
-                        Color.black,
-                        null, 4f);
-                    }
-                    Handles.DrawBezier(startPos, endPos,
-                        new Vector3(startPos.x, endPos.y, 0),
-                        new Vector3(endPos.x, startPos.y, 0),
-                        children[i].State == NodeState.Running ? Color.yellow : Color.white,
-                        null, 2f);
-                    GUILayout.Space(50);
-                }
-                EditorGUILayout.EndHorizontal();
+                    GUILayout.Space((GRAPH_SPACE_Y - 10) * 0.5f);
+                    GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(10));
+                    GUILayout.Space((GRAPH_SPACE_Y - 10) * 0.5f);
 
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(GRAPH_SPACE_X);
+                    for (int i = 0; i < children.Length; i ++)
+                    {
+                        if (i > 0) GUILayout.Space(GRAPH_SPACE_X);
+                        DrawGraphRecusively(children[i], isEditable);
+                        Rect childRect = GUILayoutUtility.GetLastRect();
+                        Vector3 startPos = new Vector3(
+                                parentRect.center.x, 
+                                parentRect.center.y + parentRect.height * 0.5f, 
+                                0);
+                        Vector3 endPos = new Vector3(
+                                childRect.center.x, 
+                                childRect.center.y - childRect.height * 0.5f, 
+                                0);
+                        if (EditorGUIUtility.isProSkin == false) //outline
+                        {
+                            Handles.DrawBezier(startPos, endPos,
+                            new Vector3(startPos.x, endPos.y, 0),
+                            new Vector3(endPos.x, startPos.y, 0),
+                            Color.black,
+                            null, 4f);
+                        }
+                        Handles.DrawBezier(startPos, endPos,
+                            new Vector3(startPos.x, endPos.y, 0),
+                            new Vector3(endPos.x, startPos.y, 0),
+                            children[i].State == NodeState.Running ? Color.yellow : Color.white,
+                            null, 2f);
+                    }
+                    GUILayout.Space(GRAPH_SPACE_X);
+                    EditorGUILayout.EndHorizontal();
+                }
                 GUILayout.FlexibleSpace();
             }
             EditorGUILayout.EndVertical();
